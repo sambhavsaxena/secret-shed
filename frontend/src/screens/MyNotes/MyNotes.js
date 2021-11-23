3e594de458074761ce9c2d1a098748749677c11f
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2021-11-23 18:26:16 +0530

    Update MyNotes.js

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { Accordion, Button, Card } from "react-bootstrap";
 import MainScreen from "../../components/MainScreen";
 import { Link } from "react-router-dom";
