79ceff12c7f56dba696f9667d9cb0ecb39ec718f
sambhavsaxena <sambhavsaxena02@gmail.com>
2022-05-20 10:53:48 +0530

    Preparing deployment

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,15 @@ import path from "path"
 import noteRoutes from "./routes/noteRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
-//Initialize app
+
 dotenv.config()
 connectDB()
 const app = express()
-//Defining routes
+
 app.use(express.json())
 app.use("/api/notes", noteRoutes)
 app.use("/api/users", userRoutes)
 
-//Deployment paths
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "/frontend/build")))
@@ -25,7 +24,6 @@ else {
   app.get("/", (req, res) => { res.send("API is running") })
 }
 
-// Error handlers
 app.use(notFound)
 app.use(errorHandler)
 
