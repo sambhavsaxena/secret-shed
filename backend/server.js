21abe2833789906c70245cfd2cc8a67f6e0a236d
Sambhav Saxena <sambhavsaxena02@gmail.com>
2022-12-24 14:07:20 +0530

    modified proxy URL

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,35 @@
-import express from "express"
-import dotenv from "dotenv"
-import connectDB from "./config/db.js"
-import path from "path"
-import articleRoutes from "./routes/articleRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import path from "path";
+import articleRoutes from "./routes/articleRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
-dotenv.config()
-connectDB()
-const app = express()
+dotenv.config();
+await connectDB();
+const app = express();
 
-app.use(express.json())
-app.use("/api/articles", articleRoutes)
-app.use("/api/users", userRoutes)
+app.use(express.json());
+app.use("/api/articles", articleRoutes);
+app.use("/api/users", userRoutes);
 
-const __dirname = path.resolve()
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "/frontend/build")))
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
 }
-else {
-  app.get("/", (req, res) => { res.send("API is running") })
-}
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 2002
+const PORT = process.env.PORT || 2002;
 
-app.listen(PORT)
-console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+app.listen(PORT);
+console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
