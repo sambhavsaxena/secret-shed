6d8dab509768984ed185f68f24a890b8e2af8c4c
Sambhav Saxena <sambhavsaxena02@gmail.com>
2022-12-24 12:10:28 +0530

    revamped database config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`)
-    process.exit()
+    console.error(`Error: ${error.message}`);
+    process.exit();
   }
-}
+};
 
-export default connectDB
+export default connectDB;
