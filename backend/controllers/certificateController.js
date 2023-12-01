7d1b351da01d794c4e3844242bcea6f1c69e1382
Sambhav Saxena <sambhavsaxena02@gmail.com>
2023-12-01 17:11:35 +0530

    fixed unique certificate entries

--- backend/controllers/certificateController.js
+++ backend/controllers/certificateController.js
@@ -2,19 +2,22 @@ import Certificate from "../models/certificateModel.js";
 import asyncHandler from "express-async-handler";
 
 const getCertificate = asyncHandler(async (req, res) => {
-  const certificate = await Certificate.find({ cid: req.query.cid });
+  const certificate = await Certificate.findOne({ cid: req.query.cid });
   if (certificate) {
     res.json(certificate);
   } else {
-    res.status(404).json({ message: "Certificate not found" });
+    res.status(404).json({ message: "Certificate not found!" });
   }
 });
 
 const CreateCertificate = asyncHandler(async (req, res) => {
   const { cid, driveURL } = req.body;
   if (!cid || !driveURL) {
-    res.status(400);
-    throw new Error("Please fill all the fields");
+    res.status(404).json({ message: "Please fill out all the fields!" });
+  }
+  const exists = await Certificate.findOne({ cid });
+  if (exists) {
+    res.status(400).json({ message: "Certificate already exists!" });
   } else {
     const certificate = new Certificate({
       cid: cid,
