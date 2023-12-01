5320098d5300fb105a721e63779faa30a6bab0cf
Sambhav Saxena <sambhavsaxena02@gmail.com>
2023-12-01 18:47:38 +0530

    fix: reverted commits and cors issues

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,15 @@ dotenv.config();
 await connectDB();
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use("/api/articles", articleRoutes);
 app.use("/api/users", userRoutes);
